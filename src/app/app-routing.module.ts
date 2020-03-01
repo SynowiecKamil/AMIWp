dc7f6561b0f0d4e39fc1386dd7e32bdcce73d1a6
Kamil Synowiec <kamilsynowiec0@gmail.com>
2020-03-01 13:17:40 +0100

    dodanie komponentow i routingu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TableComponent } from './table/table.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'table', component: TableComponent},
+  {path: 'toDoList', component: ToDoListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [TableComponent, ToDoListComponent];
