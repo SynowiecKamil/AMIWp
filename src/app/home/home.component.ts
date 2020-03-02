0e9a1ace528147d94d3a96a9c6a3a4a6a0928e92
Kamil Synowiec <kamilsynowiec0@gmail.com>
2020-03-02 16:29:23 +0100

    komponenty, moduly cd

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Title, Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  title = 'AMIW projekt';
 
-  constructor() { }
+  constructor(private titleService: Title, private meta: Meta) {
+    titleService.setTitle('');
+
+    meta.addTag({name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'}, true);
+    meta.addTag({name: 'description', content: ''}, true);
+    meta.addTag({name: 'author', content: 'Synowiec'}, true);
+
+  }
 
   ngOnInit(): void {
   }
